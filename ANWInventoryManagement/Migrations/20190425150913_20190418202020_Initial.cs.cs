aa7393cfcaab20287653918ccf87e140a5873810
Curtis Weide <curtis.weide@gmail.com>
2019-04-25 11:35:30 -0500

    Made commits to Azure DB.

---
+++ ANWInventoryManagement/Migrations/20190425150913_20190418202020_Initial.cs.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ANWInventoryManagement.Migrations
+{
+    public partial class _20190418202020_Initialcs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
